31e9a2b59ff0827e532fb7e8a9106534f6b80ba5
Krasimir Ivanov <k.ivanov29@yahoo.com>
2023-02-28 13:35:46 +0200

    Improving UserModal component

--- src/components/UI/UserModal.js
+++ src/components/UI/UserModal.js
@@ -1,53 +1,75 @@
+import { useState } from "react";
 import Spinner from "../../resources/Spinner.js";
 import Button from "./Button.js";
 import classes from "./UserModal.module.css";
 
 const UserModal = ({ modalLoading, user }) => {
-  return (
-    <div className={classes.container}>
-      <div className={classes.header}>
-        <p>{modalLoading ? "Loading..." : user.username}</p>
-      </div>
-      <div className={classes.content}>
-        {modalLoading && <Spinner w={260} h={260} />}
-        {!modalLoading && (
-          <>
-            <div className={classes.left_side}>
-              {" "}
-              <div
-                style={{
-                  backgroundImage: `url(${user.imageUrl})`,
-                }}
-                className={classes.img}
-              ></div>
-            </div>
-            <div className={classes.right_side}>
-              <div className={classes.bio_container}>
-                {" "}
-                <p className={classes.bio_header}>Bio</p>
-                <textarea
-                  className={classes.bio_text}
-                  defaultValue={
-                    user.description
-                      ? user.description
-                      : "The user have not shared any information about himself..."
-                  }
-                  readOnly={true}
-                  rows={5}
-                  cols={35}
-                />
-              </div>
+  const [sendMessageVisible, setSendMessageVisible] = useState(false);
 
-              <p className={classes.bio}>
-                <span>Email: </span>
-                {user.email}
-              </p>
-              <Button className={classes.btn}>Send message</Button>
-            </div>
-          </>
+  function sendMessageHandler() {
+    setSendMessageVisible((curr) => !curr);
+  }
+
+  return (
+    <>
+      <div className={classes.container}>
+        <div className={classes.header}>
+          <p>{modalLoading ? "Loading..." : user.username}</p>
+        </div>
+        <div className={classes.content}>
+          {modalLoading && <Spinner w={260} h={260} />}
+          {!modalLoading && (
+            <>
+              <div className={classes.left_side}>
+                {" "}
+                <div
+                  style={{
+                    backgroundImage: `url(${user.imageUrl})`,
+                  }}
+                  className={classes.img}
+                ></div>
+              </div>
+              <div className={classes.right_side}>
+                <div className={classes.bio_container}>
+                  {" "}
+                  <p className={classes.bio_header}>Bio</p>
+                  <textarea
+                    className={classes.bio_text}
+                    defaultValue={
+                      user.description
+                        ? user.description
+                        : "The user have not shared any information about himself..."
+                    }
+                    readOnly={true}
+                    rows={5}
+                    cols={35}
+                  />
+                </div>
+
+                <p className={classes.bio}>
+                  <span>Email: </span>
+                  {user.email}
+                </p>
+                <Button className={classes.btn} onClick={sendMessageHandler}>
+                  Write message
+                </Button>
+              </div>
+            </>
+          )}
+        </div>
+        {sendMessageVisible && (
+          <div className={classes.msg_container}>
+            <textarea
+              rows={3}
+              cols={28}
+              className={classes.msg_text}
+              placeholder={`Send message to ${user.username} ...`}
+            />
+            <Button>Send</Button>
+          </div>
         )}
       </div>
-    </div>
+    </>
   );
 };
 
